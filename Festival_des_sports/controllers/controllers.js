52587463c71c529b4f91057915b6d525cbb9b959
Léna Abel <lena31.abel@gmail.com>
2022-10-14 17:25:38 +0200

    Add view for association

--- Festival_des_sports/controllers/controllers.js
+++ Festival_des_sports/controllers/controllers.js
@@ -1,12 +1,17 @@
 const services = require("../services/services");
 const chalk = require("chalk");
 
+exports.association = (req, res) => {
+    console.log(chalk.green.inverse('Requete pour association reçue.'));
+    res.render('association/association.hbs', {layout: 'mainHome.hbs'});
+}
+
 exports.surnameName = (req, res) => {
-    console.log(chalk.green.inverse('Requette pour surnameName reçue.'));
+    console.log(chalk.green.inverse('Requete pour surnameName reçue.'));
     res.render('surnameName/surnameName.hbs', {layout: 'mainSurnameName.hbs'});
 }
 
 exports.home = (req, res) => {
-    console.log(chalk.green.inverse('Requette pour home reçue.'));
+    console.log(chalk.green.inverse('Requete pour home reçue.'));
     res.render('home/home.hbs', {layout: 'mainHome.hbs'});
 }
--- Festival_des_sports/routes/routes.js
+++ Festival_des_sports/routes/routes.js
@@ -4,6 +4,8 @@ const controller = require("../controllers/controllers");
 var router = express.Router();
 
 //...
+router.get("/association", controller.association);
+
 router.get("/surnameName", controller.surnameName)
 
 router.get("/home", controller.home)
