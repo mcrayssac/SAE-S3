34e5be6d6dc95dfcdbe70b2bbcb90b1ee2cac2af
mcrayssac <mcrayssa@iut-bm.univ-fcomte.fr>
2022-11-10 14:42:16 +0100

    Added sessions

--- Festival_des_sports/controllers/controllers.js
+++ Festival_des_sports/controllers/controllers.js
@@ -3,63 +3,131 @@ const chalk = require("chalk");
 
 exports.association = (req, res) => {
     console.log(chalk.green.inverse('Requete pour association reçue.'));
-    res.render('public/association/association.hbs', {layout: 'mainHome.hbs'});
+    let session = req.session; //Vérifier si une session active
+    if(session.username) {
+        res.render('public/association/association.hbs', {
+            layout: 'mainHome.hbs',
+            session: {username : session.username}
+        });
+    } else {
+        res.render('public/association/association.hbs', {layout: 'mainHome.hbs'});
+    }
 }
 
 exports.courses = (req, res) => {
     console.log(chalk.green.inverse('Requete pour courses reçue.'));
-    res.render('public/activites/competitions/courses.hbs', {
-        layout: 'mainHome.hbs',
-        filtres: services.getFiltresCourses("courses", (error, results) => {
-            if (error) return error
-            else return results
-        }),
-        courses: services.getCourses((error, results) => {
-            if (error) return error
-            else return results
-        })
-    });
+    let session = req.session; //Vérifier si une session active
+    if(session.username) {
+        res.render('public/activites/competitions/courses.hbs', {
+            layout: 'mainHome.hbs',
+            filtres: services.getFiltresCourses("courses", (error, results) => {
+                if (error) return error
+                else return results
+            }),
+            courses: services.getCourses((error, results) => {
+                if (error) return error
+                else return results
+            }),
+            session: {username : session.username}
+        });
+    } else {
+        res.render('public/activites/competitions/courses.hbs', {
+            layout: 'mainHome.hbs',
+            filtres: services.getFiltresCourses("courses", (error, results) => {
+                if (error) return error
+                else return results
+            }),
+            courses: services.getCourses((error, results) => {
+                if (error) return error
+                else return results
+            })
+        });
+    }
 }
 
 exports.restaurants = (req, res) => {
     console.log(chalk.green.inverse('Requete pour les restaurants reçue.'));
-    res.render('prestataire/restaurants/restaurants.hbs', {
-        layout: 'mainHome.hbs',
-        filtres: services.getFiltresRestaurants("restaurants", (error, results) => {
-            if (error) return error
-            else return results
-        }),
-        restaurants: services.getRestaurants((error, results) => {
-            if (error) return error
-            else return results
-        })
-    });
+    let session = req.session; //Vérifier si une session active
+    if(session.username) {
+        res.render('prestataire/restaurants/restaurants.hbs', {
+            layout: 'mainHome.hbs',
+            filtres: services.getFiltresRestaurants("restaurants", (error, results) => {
+                if (error) return error
+                else return results
+            }),
+            restaurants: services.getRestaurants((error, results) => {
+                if (error) return error
+                else return results
+            }),
+            session: {username : session.username}
+        });
+    } else {
+        res.render('prestataire/restaurants/restaurants.hbs', {
+            layout: 'mainHome.hbs',
+            filtres: services.getFiltresRestaurants("restaurants", (error, results) => {
+                if (error) return error
+                else return results
+            }),
+            restaurants: services.getRestaurants((error, results) => {
+                if (error) return error
+                else return results
+            })
+        });
+    }
 }
 
 exports.clubs = (req, res) => {
     console.log(chalk.green.inverse('Requete pour les restaurants reçue.'));
-    res.render('prestataire/clubs/clubs.hbs', {
-        layout: 'mainHome.hbs',
-        filtres: services.getFiltresClubs("restaurants", (error, results) => {
-            if (error) return error
-            else return results
-        }),
-        clubs: services.getClubs((error, results) => {
-            if (error) return error
-            else return results
-        })
-    });
+    let session = req.session; //Vérifier si une session active
+    if(session.username) {
+        res.render('prestataire/clubs/clubs.hbs', {
+            layout: 'mainHome.hbs',
+            filtres: services.getFiltresClubs("restaurants", (error, results) => {
+                if (error) return error
+                else return results
+            }),
+            clubs: services.getClubs((error, results) => {
+                if (error) return error
+                else return results
+            }),
+            session: {username : session.username}
+        });
+    } else {
+        res.render('prestataire/clubs/clubs.hbs', {
+            layout: 'mainHome.hbs',
+            filtres: services.getFiltresClubs("restaurants", (error, results) => {
+                if (error) return error
+                else return results
+            }),
+            clubs: services.getClubs((error, results) => {
+                if (error) return error
+                else return results
+            })
+        });
+    }
 }
 
 exports.club = (req, res) => {
     console.log(chalk.green.inverse('Requete pour club reçue.'));
-    res.render('prestataire/prestataires/club.hbs', {
-        layout: 'mainHome.hbs',
-        club: services.getClub(req.body.club, (error, results) =>{
-            if (error) return error
-            else return results
-        })
-    });
+    let session = req.session; //Vérifier si une session active
+    if(session.username) {
+        res.render('prestataire/prestataires/club.hbs', {
+            layout: 'mainHome.hbs',
+            club: services.getClub(req.body.club, (error, results) => {
+                if (error) return error
+                else return results
+            }),
+            session: {username : session.username}
+        });
+    } else {
+        res.render('prestataire/prestataires/club.hbs', {
+            layout: 'mainHome.hbs',
+            club: services.getClub(req.body.club, (error, results) => {
+                if (error) return error
+                else return results
+            })
+        });
+    }
 }
 
 exports.surnameName = (req, res) => {
@@ -69,13 +137,25 @@ exports.surnameName = (req, res) => {
 
 exports.home = (req, res) => {
     console.log(chalk.green.inverse('Requete pour home reçue.'));
-    res.render('home/home.hbs', {
-        layout: 'mainHome.hbs',
-        cagnotte: services.getCagnotte((error, results) => {
-            if (error) return error
-            else return results
-        })
-    });
+    let session = req.session; //Vérifier si une session active
+    if(session.username){
+        res.render('home/home.hbs', {
+            layout: 'mainHome.hbs',
+            cagnotte: services.getCagnotte((error, results) => {
+                if (error) return error
+                else return results
+            }),
+            session: {username : session.username}
+        });
+    } else {
+        res.render('home/home.hbs', {
+            layout: 'mainHome.hbs',
+            cagnotte: services.getCagnotte((error, results) => {
+                if (error) return error
+                else return results
+            })
+        }); //Si pas auth alors redirect
+    }
 }
 
 exports.signup = (req, res) => {
@@ -91,7 +171,40 @@ exports.signup = (req, res) => {
 
 exports.login = (req, res) => {
     console.log(chalk.green.inverse('Requete pour login reçue.'));
-    res.render('public/login/login.hbs', {
-        layout: 'mainHome.hbs'
+    let session = req.session;
+    console.log(session);
+    if(session.username){
+        res.locals.flashMessages.success = session.username // Modifie directement le champ dans flashmessage
+        res.redirect("/home");
+    } else {
+        res.render('public/login/login.hbs', {
+            layout: 'mainHome.hbs'
+        });
+    }
+}
+
+exports.authenticateUser = (req, res, next) => {
+    let data = {username: req.body.user_name_email, password: req.body.password};
+    services.authenticate(data, (error, results) => {
+        if (error){
+            req.flash("error", error); //Envoi un flash
+            res.locals.redirect = "/login"; //Redirect vers login
+            next();
+        }
+        req.flash("success", `${results}`);
+        res.locals.redirect = "/";
+        req.session.username = data.username; //Créer la session
+        console.log(req.session);
+        next();
     });
+}
+
+exports.redirectView = (req, res) => {
+    let redirectPath = res.locals.redirect;
+    res.redirect(redirectPath);
+}
+
+exports.logout = (req, res) => {
+    req.session.destroy();
+    res.redirect("/home");
 }
--- Festival_des_sports/routes/routes.js
+++ Festival_des_sports/routes/routes.js
@@ -1,7 +1,30 @@
 const express = require("express");
 const controller = require("../controllers/controllers");
 
+//Import module de session et cookies
+const expressSession = require("express-session");
+    cookieParser = require("cookie-parser");
+    connectFlash = require("connect-flash");
+
 var router = express.Router();
+
+//Recommander de ne pas afficher la clé secrete secret_passcode
+router.use(cookieParser("secret_passcode"))
+//Déterminer une durée de session
+const unJour = 1000 * 60 * 60 * 24 //milliseconde
+//AJoute au routeur middleware de session
+router.use(
+    expressSession({
+        secret: "secret_passcode", //Clé privée
+        cookie: {maxAge: unJour}, resave: false, saveUninitialized: false
+    })
+);
+//Ajoute un middleware
+router.use(connectFlash()); //Avec connectFlash on envoie à l'utilisateur les errors/results
+router.use((req, res, next) => {
+    res.locals.flashMessages = req.flash();
+    next();
+});
 
 //...
 // --------- PUBLIC
@@ -12,6 +35,10 @@ router.get("/association", controller.association);
 router.get("/signup", controller.signup)
 
 router.get("/login", controller.login)
+
+router.post("/login", controller.authenticateUser, controller.redirectView);
+
+router.get("/logout", controller.logout);
 
 router.get("/home", controller.home)
 
