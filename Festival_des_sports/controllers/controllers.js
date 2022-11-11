53764e2512d45f74099ec3efaafecea20d085535
mcrayssac <mcrayssa@iut-bm.univ-fcomte.fr>
2022-11-11 10:34:03 +0100

    Updated redirection of "orga"

--- Festival_des_sports/controllers/controllers.js
+++ Festival_des_sports/controllers/controllers.js
@@ -143,7 +143,11 @@ exports.orga = (req, res) => {
 exports.home = (req, res) => {
     console.log(chalk.green.inverse('Requete pour home reçue.'));
     let session = req.session; //Vérifier si une session active
-    if(session.username){
+    console.log(session);
+    if(session.username === 'admin'){
+        res.locals.flashMessages.success = session.username // Modifie directement le champ dans flashmessage
+        res.redirect("/orga");
+    } else if(session.username){
         res.render('home/home.hbs', {
             layout: 'mainHome.hbs',
             cagnotte: services.getCagnotte((error, results) => {
@@ -178,7 +182,10 @@ exports.login = (req, res) => {
     console.log(chalk.green.inverse('Requete pour login reçue.'));
     let session = req.session;
     console.log(session);
-    if(session.username){
+    if(session.username === 'admin'){
+        res.locals.flashMessages.success = session.username // Modifie directement le champ dans flashmessage
+        res.redirect("/orga");
+    } else if(session.username){
         res.locals.flashMessages.success = session.username // Modifie directement le champ dans flashmessage
         res.redirect("/home");
     } else {
@@ -205,8 +212,13 @@ exports.authenticateUser = (req, res, next) => {
 }
 
 exports.redirectView = (req, res) => {
-    let redirectPath = res.locals.redirect;
-    res.redirect(redirectPath);
+    if(req.session.username === 'admin'){
+        res.locals.flashMessages.success = req.session.username // Modifie directement le champ dans flashmessage
+        res.redirect("/orga");
+    } else {
+        let redirectPath = res.locals.redirect;
+        res.redirect(redirectPath);
+    }
 }
 
 exports.logout = (req, res) => {
