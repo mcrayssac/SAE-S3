550f2fe7f3765a1e9a259b8bd5f12dd0516aaeaf
mcrayssac <mcrayssa@iut-bm.univ-fcomte.fr>
2022-11-25 10:28:19 +0100

    Updated navbar in vue

--- vuejs/src/views/layouts/mainHomeTest.vue
+++ vuejs/src/views/layouts/mainHomeTest.vue
@@ -10,7 +10,7 @@
 
         <b-collapse class="white" id="nav-collapse" is-nav>
 
-          <b-navbar-nav left>
+          <b-navbar-nav>
             <b-nav-item href="/"><b-icon-geo-alt-fill></b-icon-geo-alt-fill> Map</b-nav-item>
 
             <b-nav-item-dropdown>
@@ -31,33 +31,40 @@
               <template #button-content><b-icon-trophy-fill></b-icon-trophy-fill> Résultats</template>
               <b-dropdown-item href="#"> Course à pied</b-dropdown-item>
               <b-dropdown-item href="#">VTT</b-dropdown-item>
-              <b-dropdown-item href="#">Vente</b-dropdown-item>
-              <b-dropdown-item href="#">Devenir prestataire</b-dropdown-item>
+              <b-dropdown-item href="#">Natation</b-dropdown-item>
+              <b-dropdown-item href="#">Course d'orientation</b-dropdown-item>
             </b-nav-item-dropdown>
 
-
+            <b-nav-item href="/association">
+              <b-iconstack>
+                <b-icon stacked icon="heart-fill" shift-v="-2.5" scale="0.5"></b-icon>
+                <b-icon stacked icon="house" scale="1.3"></b-icon>
+              </b-iconstack>
+              Association
+            </b-nav-item>
           </b-navbar-nav>
 
-          <b-navbar-nav right>
-
-            <b-nav-item-dropdown text="Lang" right>
-              <b-dropdown-item href="#">EN</b-dropdown-item>
-              <b-dropdown-item href="#">ES</b-dropdown-item>
-              <b-dropdown-item href="#">RU</b-dropdown-item>
-              <b-dropdown-item href="#">FA</b-dropdown-item>
+          <b-navbar-nav class="ms-auto">
+            <b-nav-item-dropdown v-if="session" right>
+              <template #button-content><b-icon-person-fill></b-icon-person-fill> Bonjour {{session.username}}</template>
+              <b-dropdown-item href="#">Planning</b-dropdown-item>
+              <b-dropdown-item href="#">Mes activités</b-dropdown-item>
+              <b-dropdown-item href="/logout">Déconnexion</b-dropdown-item>
+              <b-dropdown-item href="#">Supprimer le compte</b-dropdown-item>
             </b-nav-item-dropdown>
 
-            <b-nav-item-dropdown right>
-              <template #button-content>
-                <em>User</em>
-              </template>
-              <b-dropdown-item href="#">Profile</b-dropdown-item>
-              <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+            <b-nav-item-dropdown v-else right>
+              <template #button-content><b-icon-person-fill></b-icon-person-fill> Profil</template>
+              <b-dropdown-item href="/login">Se connecter</b-dropdown-item>
+              <b-dropdown-item href="/signup">S'inscrire</b-dropdown-item>
             </b-nav-item-dropdown>
 
+            <b-nav-item v-for="(item, index) in allLanguage" :key="index" right>{{item.title}} {{item.flag}}</b-nav-item>
           </b-navbar-nav>
         </b-collapse>
       </b-navbar>
+
+      <button @click="changeSession()">Change session</button>
     </div>
 </template>
 
@@ -65,10 +72,17 @@
 export default {
   name: "mainHomeTest",
   data: () => ({
+    language: "English",
+    allLanguage: [{"title":"English", "flag":""},{"title":"French", "flag":""}],
+    session: {"username": "Max"},
     backgroundNavbarColor: {"title" : 'background-color :', "body" : '#6ec8cb'}
   }),
   methods:{
-
+    changeSession(){
+      console.log("Session : "+this.session);
+      if (this.session) this.session = null
+      else this.session = {"username": "Max"}
+    }
   }
 }
 </script>
