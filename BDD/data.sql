ebb391c84564666a2800b19162561bc560483594
giuliana-fabrizio <godailfabriziogiuliana@gmail.com>
2022-12-15 14:48:45 +0100

    insert des stands

--- BDD/data.sql
+++ BDD/data.sql
@@ -1,77 +1,77 @@
-\COPY AGE (libelle_age) FROM '/home/user/Documents/BUT/2e_annee/semestre_1/SAUV_SAE-S3/AGE.csv' DELIMITER AS ',';
+\COPY AGE (libelle_age) FROM '/home/user/Documents/BUT/2e_annee/semestre_1/SAE-S3/BDD/AGE.csv' DELIMITER AS ',';
 select * from AGE;
 
-\COPY CONTRAINTE (libelle_contrainte) FROM '/home/user/Documents/BUT/2e_annee/semestre_1/SAUV_SAE-S3/CONTRAINTE.csv' DELIMITER AS ',';
+\COPY CONTRAINTE (libelle_contrainte) FROM '/home/user/Documents/BUT/2e_annee/semestre_1/SAE-S3/BDD/CONTRAINTE.csv' DELIMITER AS ',';
 select * from CONTRAINTE;
 
-\COPY PAYS (libelle_pays) FROM '/home/user/Documents/BUT/2e_annee/semestre_1/SAUV_SAE-S3/PAYS.csv' DELIMITER AS ',';
+\COPY PAYS (libelle_pays) FROM '/home/user/Documents/BUT/2e_annee/semestre_1/SAE-S3/BDD/PAYS.csv' DELIMITER AS ',';
 select * from PAYS;
 
-\COPY SEXE (libelle_sexe) FROM '/home/user/Documents/BUT/2e_annee/semestre_1/SAUV_SAE-S3/SEXE.csv' DELIMITER AS ',';
+\COPY SEXE (libelle_sexe) FROM '/home/user/Documents/BUT/2e_annee/semestre_1/SAE-S3/BDD/SEXE.csv' DELIMITER AS ',';
 select * from SEXE;
 
-\COPY TYPE_PRESTATAIRE (etat_type) FROM '/home/user/Documents/BUT/2e_annee/semestre_1/SAUV_SAE-S3/TYPE_PRESTATAIRE.csv' DELIMITER AS ',';
+\COPY TYPE_PRESTATAIRE (etat_type) FROM '/home/user/Documents/BUT/2e_annee/semestre_1/SAE-S3/BDD/TYPE_PRESTATAIRE.csv' DELIMITER AS ',';
 select * from TYPE_PRESTATAIRE;
 
-\COPY LANGUES (libelle_langue) FROM '/home/user/Documents/BUT/2e_annee/semestre_1/SAUV_SAE-S3/LANGUES.csv' DELIMITER AS ',';
+\COPY LANGUES (libelle_langue) FROM '/home/user/Documents/BUT/2e_annee/semestre_1/SAE-S3/BDD/LANGUES.csv' DELIMITER AS ',';
 select * from LANGUES;
 
-\COPY TAILLE (libelle_taille) FROM '/home/user/Documents/BUT/2e_annee/semestre_1/SAUV_SAE-S3/TAILLE.csv' DELIMITER AS ',';
+\COPY TAILLE (libelle_taille) FROM '/home/user/Documents/BUT/2e_annee/semestre_1/SAE-S3/BDD/TAILLE.csv' DELIMITER AS ',';
 select * from TAILLE;
 
-\COPY STAND (id_stand, coordonne_x, coordonne_y, rotation, id_taille) FROM '/home/user/Documents/BUT/2e_annee/semestre_1/SAUV_SAE-S3/STAND.csv' DELIMITER AS ',';
+\COPY STAND (id_stand, coordonne_x, coordonne_y, rotation, id_taille) FROM '/home/user/Documents/BUT/2e_annee/semestre_1/SAE-S3/BDD/STAND.csv' DELIMITER AS ',';
 select * from STAND;
 
-\COPY PUBLIC (prenom_public, nom_public, email_public, passwd_public, id_langue, id_age, id_sexe, id_pays) FROM '/home/user/Documents/BUT/2e_annee/semestre_1/SAUV_SAE-S3/PUBLIC.csv' DELIMITER AS ',';
+\COPY PUBLIC (prenom_public, nom_public, email_public, passwd_public, id_langue, id_age, id_sexe, id_pays) FROM '/home/user/Documents/BUT/2e_annee/semestre_1/SAE-S3/BDD/PUBLIC.csv' DELIMITER AS ',';
 select * from PUBLIC;
 
-\COPY ORGANISATEUR (nom_organisateur, email_organisateur, telephone_organisateur, passwd_organisateur) FROM '/home/user/Documents/BUT/2e_annee/semestre_1/SAUV_SAE-S3/ORGANISATEUR.csv' DELIMITER AS ',';
+\COPY ORGANISATEUR (nom_organisateur, email_organisateur, telephone_organisateur, passwd_organisateur) FROM '/home/user/Documents/BUT/2e_annee/semestre_1/SAE-S3/BDD/ORGANISATEUR.csv' DELIMITER AS ',';
 select * from ORGANISATEUR;
 
-\COPY LIEU (libelle_lieu) FROM '/home/user/Documents/BUT/2e_annee/semestre_1/SAUV_SAE-S3/LIEU.csv' DELIMITER AS ',';
+\COPY LIEU (libelle_lieu) FROM '/home/user/Documents/BUT/2e_annee/semestre_1/SAE-S3/BDD/LIEU.csv' DELIMITER AS ',';
 select * from LIEU;
 
-\COPY SPORT (libelle_sport) FROM '/home/user/Documents/BUT/2e_annee/semestre_1/SAUV_SAE-S3/SPORT.csv' DELIMITER AS ',';
+\COPY SPORT (libelle_sport) FROM '/home/user/Documents/BUT/2e_annee/semestre_1/SAE-S3/BDD/SPORT.csv' DELIMITER AS ',';
 select * from SPORT;
 
-\COPY PERIODE FROM '/home/user/Documents/BUT/2e_annee/semestre_1/SAUV_SAE-S3/PERIODE.csv' DELIMITER AS ',';
+\COPY PERIODE FROM '/home/user/Documents/BUT/2e_annee/semestre_1/SAE-S3/BDD/PERIODE.csv' DELIMITER AS ',';
 select * from PERIODE;
 
-\COPY RESERVATION (date_periode, id_public) FROM '/home/user/Documents/BUT/2e_annee/semestre_1/SAUV_SAE-S3/RESERVATION.csv' DELIMITER AS ',';
+\COPY RESERVATION (date_periode, id_public) FROM '/home/user/Documents/BUT/2e_annee/semestre_1/SAE-S3/BDD/RESERVATION.csv' DELIMITER AS ',';
 select * from RESERVATION;
 
-\COPY CARACTERISTIQUE (libelle_caracteristique, id_type) FROM '/home/user/Documents/BUT/2e_annee/semestre_1/SAUV_SAE-S3/CARACTERISTIQUE.csv' DELIMITER AS ',';
+\COPY CARACTERISTIQUE (libelle_caracteristique, id_type) FROM '/home/user/Documents/BUT/2e_annee/semestre_1/SAE-S3/BDD/CARACTERISTIQUE.csv' DELIMITER AS ',';
 select * from CARACTERISTIQUE;
 
-\COPY PRESTATAIRE (nom_prestataire, email_prestataire, site_web_prestataire, telephone_prestataire, passwd_prestataire, etat_inscription, id_stand, id_type) FROM '/home/user/Documents/BUT/2e_annee/semestre_1/SAUV_SAE-S3/PRESTATAIRE.csv' DELIMITER AS ',';
+\COPY PRESTATAIRE (nom_prestataire, email_prestataire, site_web_prestataire, telephone_prestataire, passwd_prestataire, etat_inscription, id_stand, id_type) FROM '/home/user/Documents/BUT/2e_annee/semestre_1/SAE-S3/BDD/PRESTATAIRE.csv' DELIMITER AS ',';
 select * from PRESTATAIRE;
 
-\COPY COURSES (libelle_course, nb_km, nb_place, prix, date_periode, id_sport, id_lieu) FROM '/home/user/Documents/BUT/2e_annee/semestre_1/SAUV_SAE-S3/COURSES.csv' DELIMITER AS ',';
+\COPY COURSES (libelle_course, nb_km, nb_place, prix, date_periode, id_sport, id_lieu) FROM '/home/user/Documents/BUT/2e_annee/semestre_1/SAE-S3/BDD/COURSES.csv' DELIMITER AS ',';
 select * from COURSES;
 
-\COPY INITIATION (libelle_initiation, etat_initiation, date_periode, id_lieu, id_prestataire) FROM '/home/user/Documents/BUT/2e_annee/semestre_1/SAUV_SAE-S3/INITIATION.csv' DELIMITER AS ',';
+\COPY INITIATION (libelle_initiation, etat_initiation, date_periode, id_lieu, id_prestataire) FROM '/home/user/Documents/BUT/2e_annee/semestre_1/SAE-S3/BDD/INITIATION.csv' DELIMITER AS ',';
 select * from INITIATION;
 
-\COPY NOTE (libelle_note, id_prestataire, id_public) FROM '/home/user/Documents/BUT/2e_annee/semestre_1/SAUV_SAE-S3/NOTE.csv' DELIMITER AS ',';
+\COPY NOTE (libelle_note, id_prestataire, id_public) FROM '/home/user/Documents/BUT/2e_annee/semestre_1/SAE-S3/BDD/NOTE.csv' DELIMITER AS ',';
 select * from NOTE;
 
-\COPY COMMENTAIRE (libelle_commentaire, id_prestataire, id_public) FROM '/home/user/Documents/BUT/2e_annee/semestre_1/SAUV_SAE-S3/COMMENTAIRE.csv' DELIMITER AS ',';
+\COPY COMMENTAIRE (libelle_commentaire, id_prestataire, id_public) FROM '/home/user/Documents/BUT/2e_annee/semestre_1/SAE-S3/BDD/COMMENTAIRE.csv' DELIMITER AS ',';
 select * from COMMENTAIRE;
 
-\COPY Emet FROM '/home/user/Documents/BUT/2e_annee/semestre_1/SAUV_SAE-S3/Emet.csv' DELIMITER AS ',';
+\COPY Emet FROM '/home/user/Documents/BUT/2e_annee/semestre_1/SAE-S3/BDD/Emet.csv' DELIMITER AS ',';
 select * from Emet;
 
-\COPY Possede FROM '/home/user/Documents/BUT/2e_annee/semestre_1/SAUV_SAE-S3/Possede.csv' DELIMITER AS ',';
+\COPY Possede FROM '/home/user/Documents/BUT/2e_annee/semestre_1/SAE-S3/BDD/Possede.csv' DELIMITER AS ',';
 select * from Possede;
 
-\COPY Participe FROM '/home/user/Documents/BUT/2e_annee/semestre_1/SAUV_SAE-S3/Participe.csv' DELIMITER AS ',';
+\COPY Participe FROM '/home/user/Documents/BUT/2e_annee/semestre_1/SAE-S3/BDD/Participe.csv' DELIMITER AS ',';
 select * from Participe;
 
-\COPY A_Propos FROM '/home/user/Documents/BUT/2e_annee/semestre_1/SAUV_SAE-S3/A_Propos.csv' DELIMITER AS ',';
+\COPY A_Propos FROM '/home/user/Documents/BUT/2e_annee/semestre_1/SAE-S3/BDD/A_Propos.csv' DELIMITER AS ',';
 select * from A_Propos;
 
-\COPY Pour FROM '/home/user/Documents/BUT/2e_annee/semestre_1/SAUV_SAE-S3/Pour.csv' DELIMITER AS ',';
+\COPY Pour FROM '/home/user/Documents/BUT/2e_annee/semestre_1/SAE-S3/BDD/Pour.csv' DELIMITER AS ',';
 select * from Pour;
 
-\COPY Detient FROM '/home/user/Documents/BUT/2e_annee/semestre_1/SAUV_SAE-S3/Detient.csv' DELIMITER AS ',';
+\COPY Detient FROM '/home/user/Documents/BUT/2e_annee/semestre_1/SAE-S3/BDD/Detient.csv' DELIMITER AS ',';
 select * from Detient;
