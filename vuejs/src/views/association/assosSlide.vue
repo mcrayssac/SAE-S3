de2ba8d3feb60e2e0218f6d8f9649f96ab92cce3
Léna Abel <lena31.abel@gmail.com>
2022-12-22 11:52:33 +0100

    Updated association

---
+++ vuejs/src/views/association/assosSlide.vue
@@ -0,0 +1,33 @@
+<template>
+  <vueper-slides fade ref="myVueperSlides2"
+                 style="background-size:100% auto; background-position: center; background-repeat: no-repeat;"
+                 :autoplay="true"
+                 :parallax="1"
+                 :parallax-fixed-content="true"
+                 :arrows="false"
+                 :bullets="false"
+                 :duration="5000"
+                 :pauseOnHover="false"
+                 :touchable="false"
+                 :fixed-height="true"
+  >
+    <vueper-slide v-for="(item, index) in slide" :key="index" :image="item"
+                  title="L'HISTOIRE D'APF FRANCE"/>
+  </vueper-slides>
+</template>
+
+<script>
+import { VueperSlides, VueperSlide } from 'vueperslides'
+import 'vueperslides/dist/vueperslides.css'
+export default {
+  name: "assosSlide",
+  components:{ VueperSlides, VueperSlide},
+  props: {
+    slide: Array,
+  }
+}
+</script>
+
+<style scoped>
+  @import '../../../public/css/home/homeSlideMain/homeSlideMain.css';
+</style>
