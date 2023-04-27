19cf67d68c95dd83981a29326f6e8c87bb035450
Léna Abel <lena31.abel@gmail.com>
2023-04-27 23:43:01 +0200

    Modified public script

--- BDD/script.sql
+++ BDD/script.sql
@@ -1,5 +1,6 @@
 /* ====== accepte ====== */
 /* ====== id_organisateur dans courses ====== */
+/* \i 'C:/Users/lena3/OneDrive/Documents/IUT_Windows/S3/SAE-S3/BDD/data.sql' */
 SET
 CLIENT_ENCODING TO 'UTF8';
 DROP TABLE IF EXISTS Correspond;
@@ -20,6 +21,7 @@ DROP TABLE IF EXISTS PERIODE;
 DROP TABLE IF EXISTS LIEU;
 DROP TABLE IF EXISTS SPORT;
 DROP TABLE IF EXISTS PRESTATAIRE;
+DROP TABLE IF EXISTS AFFLUENCE;
 DROP TABLE IF EXISTS STAND;
 DROP TABLE IF EXISTS FAMILLE;
 DROP TABLE IF EXISTS TAILLE;
@@ -111,6 +113,7 @@ CREATE TABLE PUBLIC
     id_age        INT NOT NULL,
     id_sexe       INT NOT NULL,
     id_pays       INT NOT NULL,
+    google_id    VARCHAR(100),
     CONSTRAINT pk_public PRIMARY KEY (id_public),
     CONSTRAINT fk_langue_public FOREIGN KEY (id_langue) REFERENCES LANGUES (id_langue),
     CONSTRAINT fk_age_public FOREIGN KEY (id_age) REFERENCES AGE (id_age),
@@ -168,6 +171,13 @@ CREATE TABLE CARACTERISTIQUE
     CONSTRAINT fk_famille_caracteristique FOREIGN KEY (id_famille) REFERENCES FAMILLE (id_famille)
 );
 
+CREATE TABLE AFFLUENCE
+(
+    id_affluence          SERIAL,
+    libelle_affluence     VARCHAR(100),
+    CONSTRAINT pk_affluence PRIMARY KEY (id_affluence)
+);
+
 
 CREATE TABLE PRESTATAIRE
 (
@@ -184,8 +194,10 @@ CREATE TABLE PRESTATAIRE
     text_droite           TEXT,
     url_image           TEXT,
     image_body            TEXT,
+    id_affluence          INT,
     CONSTRAINT pk_prestataire PRIMARY KEY (id_prestataire),
     CONSTRAINT fk_stand_prestataire FOREIGN KEY (id_stand) REFERENCES STAND (id_stand),
+    CONSTRAINT fk_affluence_prestataire FOREIGN KEY (id_affluence) REFERENCES AFFLUENCE (id_affluence),
     CONSTRAINT fk_type_prestataire FOREIGN KEY (id_type) REFERENCES TYPE_PRESTATAIRE (id_type)
 );
 
